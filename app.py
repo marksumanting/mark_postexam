773c06517e6664170004289287b822b17212dc5a
KlemensLim <69836997+KlemensLim@users.noreply.github.com>
2020-10-22 13:33:10 +0800

    Added a certain line (extra feeling)

--- app.py
+++ app.py
@@ -27,18 +27,19 @@ while True:
       feelings_list.append("relieved")
       encouragement_list.append("Relax after the stressful examinations")
       counter += 1
+
     if each_word == "dead tired":
       feelings_list.append("dead tired")
       encouragement_list.append("Just find something fun to do")
       counter += 1
-    if each_word == "sad":
+     if each_word == "sad":
       feelings_list.append("sad")
       encouragement_list.append("talk to your friends")
       counter += 1  
-    if each_word == "relaxed":
-      feelings_list.append("relaxed")
-      encouragement_list.append("Good for you!")
-      counter += 1 
+    if each_word == "proud":
+      feelings_list.append("proud")
+      encouragement_list.append("not to be complacent and keep up the good work!")
+      counter += 1  
       
     if counter == 0:
 
